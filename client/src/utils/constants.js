4fd85b845305e1537dbd735cc34252598ee3e349
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-05-31 19:11:16 +0700

    complete 1.3

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
 export const GRAPHQL_SERVER = 'https://mern-note-ch2m.onrender.com'
-export const GRAPHQL_WS_SERVER = 'ws://mern-note-ch2m.onrender.com'
+export const GRAPHQL_WS_SERVER = 'wss://mern-note-ch2m.onrender.com'
